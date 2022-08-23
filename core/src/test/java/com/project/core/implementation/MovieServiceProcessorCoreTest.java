82952f5c0aa30df8e6b610d7b6efd1c99a632882
Daniel Ivanov <80488757+DanielI22@users.noreply.github.com>
2022-08-23 18:56:49 +0300

    Basic

---
+++ core/src/test/java/com/project/core/implementation/MovieServiceProcessorCoreTest.java
@@ -0,0 +1,19 @@
+package com.project.core.implementation;
+
+import com.project.core.exception.MovieNotFoundException;
+import com.project.core.interfaces.MovieServiceProcessor;
+import com.project.data.externalmodel.MovieAPIResponse;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MovieServiceProcessorCoreTest {
+
+}
